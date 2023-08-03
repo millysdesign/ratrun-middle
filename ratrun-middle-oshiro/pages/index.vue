ddf0175f8d79ec2d825f7418bd1c3050b402496f
SEIKO OSHIRO <milly@seikonoiMac.local>
2023-08-03 17:16:36 +0900

    経営理念の作成

--- ratrun-middle-oshiro/pages/index.vue
+++ ratrun-middle-oshiro/pages/index.vue
@@ -3,10 +3,11 @@
     <div class="top-image">
       <div class="top-description">
         <h1>理念の文章が入ります</h1>
-        <p>母体が内装解体専門会社であるからこそ、一般的な施工会社 <br>
-          よりも解体費用を抑えることができます。つまり、解体費用を  <br>
-          抑えることで、余力のある資金が生まれ、使い道を広げることが <br>
-          できるということです。<br>
+        <p>
+          母体が内装解体専門会社であるからこそ、一般的な施工会社 <br />
+          よりも解体費用を抑えることができます。つまり、解体費用を <br />
+          抑えることで、余力のある資金が生まれ、使い道を広げることが <br />
+          できるということです。<br />
           選択のできる予算の使途も合わせてご提案いたします。
         </p>
       </div>
@@ -14,8 +15,131 @@
     <!-- 事業目的のボタン手前まで書く -->
     <!-- main-title -->
     <div></div>
+
     <!-- philosophy -->
-    <div></div>
+    <div class="philosophy">
+      <div>
+        <h2 class="section-title">経営理念 PHILOSOPHY</h2>
+      </div>
+
+      <div class="philosophy-top">
+        <div class="philosophy-top-left">
+          <img src="@/assets/img/philosophy.png" alt="" />
+        </div>
+        <div class="philosophy-top-right">
+          <p>テキストが入ります。テキストが入ります。</p>
+          <p>
+            テキストが入ります。テキストが入ります。テキストが入ります。
+            テキストが入ります。テキストが入ります。テキストが入ります。
+            テキストが入ります。テキストが入ります。テキストが入ります。
+            テキストが入ります。テキストが入ります。テキストが入ります。
+            テキストが入ります。テキストが入ります。テキストが入ります。
+            テキストが入ります。テキストが入ります。テキストが入ります。
+            テキストが入ります。テキストが入ります。テキストが入ります。
+            テキストが入ります。テキストが入ります。テキストが入ります。
+            テキストが入ります。テキストが入ります。テキストが入ります。
+            テキストが入ります。
+          </p>
+        </div>
+      </div>
+
+      <div class="philosophy-second">
+        <div class="philosophy-second-left">
+          <p>テキストが入ります。テキストが入ります。</p>
+          <p>
+            テキストが入ります。テキストが入ります。テキストが入ります。
+            テキストが入ります。テキストが入ります。テキストが入ります。
+            テキストが入ります。テキストが入ります。テキストが入ります。
+            テキストが入ります。テキストが入ります。テキストが入ります。
+            テキストが入ります。テキストが入ります。テキストが入ります。
+            テキストが入ります。テキストが入ります。テキストが入ります。
+            テキストが入ります。テキストが入ります。テキストが入ります。
+            テキストが入ります。テキストが入ります。テキストが入ります。
+            テキストが入ります。テキストが入ります。テキストが入ります。
+            テキストが入ります。
+          </p>
+        </div>
+        <div class="philosophy-second-right">
+          <img src="@/assets/img/philosophy.png" alt="" />
+        </div>
+      </div>
+
+      <div class="philosophy-third">
+        <div class="philosophy-third-left">
+          <img src="@/assets/img/philosophy.png" alt="" />
+          <p>テキストが入ります。テキストが入ります。</p>
+          <p>
+            テキストが入ります。テキストが入ります。テキストが入ります。
+            テキストが入ります。テキストが入ります。テキストが入ります。
+            テキストが入ります。テキストが入ります。テキストが入ります。
+            テキストが入ります。テキストが入ります。テキストが入ります。
+            テキストが入ります。テキストが入ります。テキストが入ります。
+            テキストが入ります。テキストが入ります。テキストが入ります。
+            テキストが入ります。テキストが入ります。
+          </p>
+        </div>
+
+        <div class="philosophy-third-right">
+          <img src="@/assets/img/philosophy.png" alt="" />
+          <p>テキストが入ります。テキストが入ります。</p>
+          <p>
+            テキストが入ります。テキストが入ります。テキストが入ります。
+            テキストが入ります。テキストが入ります。テキストが入ります。
+            テキストが入ります。テキストが入ります。テキストが入ります。
+            テキストが入ります。テキストが入ります。テキストが入ります。
+            テキストが入ります。テキストが入ります。テキストが入ります。
+            テキストが入ります。テキストが入ります。テキストが入ります。
+            テキストが入ります。テキストが入ります。
+          </p>
+        </div>
+
+        <div class="philosophy-fourth">
+          <div class="philosophy-fourth-left">
+            <p>テキストが入ります。テキストが入ります。</p>
+            <p>
+              テキストが入ります。テキストが入ります。テキストが入ります。
+              テキストが入ります。テキストが入ります。テキストが入ります。
+              テキストが入ります。テキストが入ります。テキストが入ります。
+              テキストが入ります。テキストが入ります。テキストが入ります。
+              テキストが入ります。テキストが入ります。テキストが入ります。
+              テキストが入ります。テキストが入ります。テキストが入ります。
+              テキストが入ります。テキストが入ります。
+            </p>
+          </div>
+          <div class="philosophy-fourth-right">
+            <table class="philosophy">
+              <tr>
+                <th>竣工年：</th>
+                <td>2017年</td>
+              </tr>
+              <tr>
+                <th>計画地：</th>
+                <td>愛知県名古屋市</td>
+              </tr>
+              <tr>
+                <th>構造・面積：</th>
+                <td>木造2階建　96㎡</td>
+              </tr>
+              <tr>
+                <th>工期：</th>
+                <td>2017年9月〜11月</td>
+              </tr>
+              <tr>
+                <th>業務内容：</th>
+                <td>
+                  インテリアデザイン、施工マネ ジメント イOXOXOXOXOXOXOX OXOXOX
+                </td>
+              </tr>
+              <tr>
+                <th>クライアント：</th>
+                <td>個人</td>
+              </tr>
+            </table>
+          </div>
+        </div>
+      </div>
+    </div>
+
     <!-- vision -->
     <div></div>
   </div>
@@ -45,5 +169,33 @@ export default {};
   top: 30%;
   margin-right: 100px;
   color: #fff;
+}
+
+// philosophy
+.philosophy {
+}
+
+.philosophy-top {
+  display: flex;
+  justify-content: space-between;
+}
+
+.philosophy-top-left {
+  width: 48%;
+}
+.philosophy-top-right {
+  width: 48%;
+}
+
+.philosophy-second {
+  display: flex;
+  justify-content: space-between;
+}
+
+.philosophy-second-left {
+  width: 48%;
+}
+.philosophy-second-right {
+  width: 48%;
 }
 </style>
